cbac341c30edf72d7e1ac891404f5a2a00ee0168
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2026-09-07 17:24:19 +0200

    optmize longer

--- mutsel_tools/src/main.rs
+++ mutsel_tools/src/main.rs
@@ -39,5 +39,5 @@ fn main() {
 
     model.log_branch_length.set(&Tensor::zeros(&[distances.dim(0).unwrap()], F64, &candle_core::Device::Cpu).unwrap()).unwrap();
 
-    mutsel_rust::optimization::optimize(&model, 100, 1000, 1e-6, 5, mutsel_rust::Verbosity::Med, &"debug");
+    mutsel_rust::optimization::optimize(&model, 100, 1000, 1e-7, 5, mutsel_rust::Verbosity::Med, &"debug");
 }
